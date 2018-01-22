4fe5bf103dcf9b6a6406c824813493d7ef97bb67
Jared Lera <jared@autofi.io>
2018-01-21 17:55:37 -0800

    added more reports

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 /*
 TODOS:
 - FIX cors issue and deploy to bogoin.herokuapp.com
-- TEST on http://nether.space/reporter.html to see if we can post
-- TEST on 상태.com to see if cors is really working
-- Add logic to block requests using the wrong keys (Make sure we put a timer on this)
+✅ TEST on http://nether.space/reporter.html to see if we can post
+❌ TEST on 상태.com to see if cors is really working
+✅ Update Nether.Space to:
+	✅ send a key from local storage inside the header
+	- 
+✅ Add logic to block requests using the wrong keys (Make sure we put a timer on this)
 - Add cors to the route, and only allow from nether space for now
 - Structure the response in a way that plot.ly likes
  */
@@ -12,12 +15,84 @@ var express = require('express')
 	, bodyParser = require('body-parser')
 	, cors = require('cors')
 	, http = require('http')
+	, async = require('async')
+	, mongoose = require('mongoose')
+	, loanAppModels = require('@autofidev/models')
+	, PandaModels = require('@autofidev/panda-models')
+	, encrypt = require('@autofidev/encrypt')
+	, configs = require('./_config.json')
 	, _ = require('lodash')
 ;
 
 var app = express()
 	, port = parseInt(process.env.PORT || '3272')
+	, dbUri = process.env.MONGO_DB || configs.dbUri
+	, dbs = {
+		local: {
+			uri: process.env.MONGO_DB_l || configs.dbUri_l
+			, mongoose: new mongoose.Mongoose()
+		}
+		, staging: {
+			uri: process.env.MONGO_DB_s || configs.dbUri_s
+			, mongoose: new mongoose.Mongoose()
+		}
+		, production: {
+			uri: process.env.MONGO_DB_f || configs.dbUri_f
+			, mongoose: new mongoose.Mongoose()
+		}
+		, panda: {
+			uri: process.env.MONGO_DB_panda || configs.pandaDbUri
+			, mongoose: new mongoose.Mongoose()
+		}
+	}
+	, encryption = encrypt('4uWhEm+%9#_wT6UM=ngC4@n+R6KF7P')
 ;
+
+if (mongoose.connection.readyState !== 1) {
+	mongoose.connect(dbUri, {useMongoClient: true});
+}
+
+// if (pandaMongoose.connection.readyState !== 1) {
+// 	pandaMongoose.connect(pandaDbUri, {useMongoClient: true});
+// }
+
+//var pandaModels = new PandaModels(pandaMongoose);
+
+function convertDates(obj) {
+	if(!obj) return;
+	if(typeof(obj) !== 'object') return;
+	if(Array.isArray(obj)) {
+		obj.forEach(i => {
+			convertDates(i)
+		})
+	}
+	Object.keys(obj).forEach(k => {
+		if(!obj[k]) return;
+		if(typeof(obj[k] === 'string')) {
+			if(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(obj[k])) {
+				obj[k] = new Date(obj[k])
+			}
+		} 
+		if(typeof(obj[k]) === 'object') {
+			convertDates(obj[k])
+		}
+	})
+}
+
+function decrypt(obj, keys) {
+	if(!obj) return;
+	if(Array.isArray(obj)) {
+		obj.forEach(i => decrypt(i, keys));
+		return;
+	}
+	if(typeof(obj) !== 'object') return;
+	keys.forEach(key => {
+		var val = _.get(obj, key);
+		if(!val) return;
+		_.set(obj, key, encryption.decrypt(val))
+	});
+}
+
 
 app.use(bodyParser.json({limit: '16mb'}));
 
@@ -42,12 +117,56 @@ app.use(cors(function(req, cb) {
 	cb(null, corsOptions);
 }));
 
+app.use(function(req,res,next) {
+	if(req.hostname === 'localhost') return next();
+	if(req.headers.secret === configs.secret)
+		return next();
+	res.status(401);
+	res.send('Not Authorized');
+})
+
 
 app.options('/query', cors(), (req, res, next) => {
-	res.send(req.body);
+	res.send();
 })
 app.post('/query', cors(), (req, res, next) => {
-	res.send(req.body);
+	if(!req || !req.body) return res.send('BAD');
+
+	var results = []
+		, request = req.body
+		, keysToDecrypt = request.decrypt ? Object.assign({}, request.decrypt) : []
+		, environment = request.environment
+	;
+	delete request.decrypt;
+	delete request.environment;
+
+	convertDates(request);
+	async.eachSeries(Object.keys(request), function(key, cb) {
+		
+		var system = request[key].system
+			, models = system === 'panda' ? pandaModels : loanAppModels;
+		delete request[key].system;
+
+		if(!models[key]) return cb();
+		var cmd = models[key]
+			, actions = request[key]
+		;
+
+		Object.keys(actions).forEach(action => {
+			console.log(action)
+			if(action === 'exec' || !cmd[action]) return;
+			cmd = cmd[action](actions[action]);
+		})
+		cmd.exec(function(err, resp) {
+			console.log('ERROR'. err)
+			if(resp && keysToDecrypt && keysToDecrypt.keys && keysToDecrypt.keys.length) decrypt(resp, keysToDecrypt.keys);
+			results.push(err || resp);
+			cb(err, resp);
+		});
+	}, function(err) {
+		res.send(err || results);
+	})
+	
 })
 
 var server = http.createServer(app);
